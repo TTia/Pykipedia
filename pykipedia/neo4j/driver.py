bd730d3bdf87058386cab33c621b929823e8e3d0
Mattia Barrasso <mattia.barrasso@gmail.com>
2013-10-17 18:49:26 +0200

    Definizione della struttura

--- pykipedia/neo4j/driver.py
+++ pykipedia/neo4j/driver.py
@@ -7,28 +7,50 @@ from py2neo import neo4j
 
 class Driver:
 	
-	
 	def __defineConstraint__(self):
 		pass
+	
 	def __defineIndex__(self):
 		pass
-	def createNode(self):
+	
+	def __getConnection(self):
+		return neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
+	
+	def initDb(self):
 		pass
+	
+	def createNode(self, title):
+		'''
+		Crea un nuovo nodo sul db.
+		:param title: titolo della voce di Wikipedia
+		'''
+		graph_db = self.__getConnection()
+		query_text = "CREATE UNIQUE (p:Page{title:'%s'});" % title
+		query = neo4j.CypherQuery(graph_db, query_text)
+		query.run()
+	
+	def createEdge(self, startTitle, endTitle):
+		'''
+		Crea un nuovo arco orientato sul db.
+		:param startTitle: titolo della voce che contiene il link.
+		:param endTitle: titolo della voce a cui il link si riferisce. 
+		'''
+		pass
+	
 	def createMultipleNodes(self):
 		pass
-	def createEdge(self):
-		pass
+	
 	def createMultipleEdges(self):
 		pass
 	
-	def testConnection(self):
-		graphDb = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
-		graphDb.neo4j_version()
-		return "!"
-		#print(a,b,c,d)
-		
-graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
-query = neo4j.CypherQuery(graph_db, "MATCH (t:Tag) RETURN t;")
-for record in query.stream():
-	print(record[0])
-print("Done")
+	def __queryExample(self, label="Page"):
+		graph_db = self.__getConnection()
+		query_text = "MATCH (n:%s) RETURN n;" % label
+		query = neo4j.CypherQuery(graph_db, query_text)
+		# for record in query.stream():
+		# 	print record[0]
+
+
+
+
+
